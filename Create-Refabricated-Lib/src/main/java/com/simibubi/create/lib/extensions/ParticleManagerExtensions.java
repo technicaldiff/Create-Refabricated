87168d96656eca427da6340cc729444bb32620bd
PepperBell <44146161+PepperCode1@users.noreply.github.com>
2021-04-07 18:21:39 -0700

    Fix broken checkstyle link

--- Create-Refabricated-Lib/src/main/java/com/simibubi/create/lib/extensions/ParticleManagerExtensions.java
+++ Create-Refabricated-Lib/src/main/java/com/simibubi/create/lib/extensions/ParticleManagerExtensions.java
@@ -7,6 +7,7 @@ import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleType;
 
+@Deprecated
 public interface ParticleManagerExtensions {
 	@ApiStatus.Internal
 	<T extends ParticleEffect> void create$registerFactory0(ParticleType<T> particleType, ParticleManager.SpriteAwareFactory<T> spriteAwareFactory);
