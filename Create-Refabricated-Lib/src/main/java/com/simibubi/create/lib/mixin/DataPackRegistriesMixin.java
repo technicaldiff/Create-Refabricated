a26a003825fbcbc80b1246c7e3b1fd5327dd76d4
Platymemo <edwardhazelton+github@gmail.com>
2021-05-31 01:59:35 -0800

    resource reload listeners should now register properly

--- Create-Refabricated-Lib/src/main/java/com/simibubi/create/lib/mixin/DataPackRegistriesMixin.java
+++ Create-Refabricated-Lib/src/main/java/com/simibubi/create/lib/mixin/DataPackRegistriesMixin.java
@@ -1,6 +1,8 @@
 package com.simibubi.create.lib.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,12 +12,20 @@ import com.simibubi.create.lib.utility.MixinHelper;
 
 import net.minecraft.command.Commands;
 import net.minecraft.resources.DataPackRegistries;
+import net.minecraft.resources.IFutureReloadListener;
+import net.minecraft.resources.IReloadableResourceManager;
 
 @Mixin(DataPackRegistries.class)
 public abstract class DataPackRegistriesMixin {
+	@Shadow
+	@Final
+	private IReloadableResourceManager resourceManager;
+
 	@Inject(at = @At("TAIL"),
 			method = "<init>(Lnet/minecraft/command/Commands$EnvironmentType;I)V")
 	public void create$DataPackRegistries(Commands.EnvironmentType environmentType, int i, CallbackInfo ci) {
-		DataPackReloadCallback.EVENT.invoker().onDataPackReload(MixinHelper.cast(this));
+		for (IFutureReloadListener listener : DataPackReloadCallback.EVENT.invoker().onDataPackReload(MixinHelper.cast(this))) {
+			resourceManager.addReloadListener(listener);
+		}
 	}
 }
