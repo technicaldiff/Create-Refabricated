733997d113b4f360ee84a44a4effa6e5860ae734
Tropheus Jay <jverrellijr@icloud.com>
2021-06-11 09:35:21 -0400

    fix GUIs not opening

--- Create-Refabricated-Lib/src/main/java/com/simibubi/create/lib/utility/NetworkUtil.java
+++ Create-Refabricated-Lib/src/main/java/com/simibubi/create/lib/utility/NetworkUtil.java
@@ -2,50 +2,33 @@ package com.simibubi.create.lib.utility;
 
 import java.util.function.Consumer;
 
-import com.simibubi.create.lib.mixin.accessor.ServerPlayerEntityAccessor;
-
-import io.netty.buffer.Unpooled;
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.Container;
-import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.network.play.server.SOpenWindowPacket;
+import net.minecraft.util.text.ITextComponent;
 
 public class NetworkUtil {
-	/**
-	 * This is a poorly stitched together mix of forge's NetworkHooks.openGui() and {@link ServerPlayerEntity#openContainer}
-	 */
+
 	public static void openGUI(ServerPlayerEntity player, INamedContainerProvider containerProvider, Consumer<PacketBuffer> extraDataWriter) {
-		if (player.world.isRemote) return;
-		player.closeContainer();
-		((ServerPlayerEntityAccessor) player).callGetNextWindowId();
-		int openContainerId = ((ServerPlayerEntityAccessor) player).getCurrentWindowId();
-		PacketBuffer extraData = new PacketBuffer(Unpooled.buffer());
-		extraDataWriter.accept(extraData);
-		extraData.readerIndex(0);
+		player.openContainer(new ExtendedScreenHandlerFactory() {
+			@Override
+			public ITextComponent getDisplayName() {
+				return containerProvider.getDisplayName();
+			}
 
-		PacketBuffer output = new PacketBuffer(Unpooled.buffer());
-		output.writeVarInt(extraData.readableBytes());
-		output.writeBytes(extraData);
+			@Override
+			public Container createMenu(int arg0, PlayerInventory arg1, PlayerEntity arg2) {
+				return containerProvider.createMenu(arg0, arg1, arg2);
+			}
 
-		if (output.readableBytes() > 32600 || output.readableBytes() < 1) {
-			throw new IllegalArgumentException("Invalid PacketBuffer for openGui, found "+ output.readableBytes()+ " bytes");
-		}
-
-		Container c = containerProvider.createMenu(openContainerId, player.inventory, player);
-		ContainerType<?> type = c.getType();
-
-		Container container = containerProvider.createMenu(openContainerId, player.inventory, player);
-		if (container == null) {
-			return;
-		}
-
-		player.connection.sendPacket(new SOpenWindowPacket(container.windowId, container.getType(), containerProvider.getDisplayName()));
-		container.addListener(player);
-		player.openContainer = container;
-
-		player.openContainer = c;
-		player.openContainer.addListener(player);
+			@Override
+			public void writeScreenOpeningData(ServerPlayerEntity player, PacketBuffer buf) {
+				extraDataWriter.accept(buf);
+			}
+		});
 	}
 }
