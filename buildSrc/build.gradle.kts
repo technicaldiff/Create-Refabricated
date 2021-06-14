9c8989b3d41f39cc35efbe6e66fd3afe5ae15b9f
PepperBell <44146161+PepperCode1@users.noreply.github.com>
2021-06-13 22:49:21 -0700

    Try to add Quiltflower
    It doesn't actually work

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,16 +1,20 @@
 plugins {
-	java
+	`java`
 }
 
 repositories {
 	jcenter()
 	mavenCentral()
-	maven("https://maven.shedaniel.me/")
 	maven("https://maven.fabricmc.net/")
+	maven("https://server.bbkr.space/artifactory/libs-release/") {
+		name = "Cotton"
+	}
 	maven("https://jitpack.io/")
+	maven("https://maven.shedaniel.me/")
 }
 
 dependencies {
-	implementation("com.github.PepperCode1", "mcp-tiny", "1bde92310cd80f0d834403d40219a2a06be431da")
 	implementation("net.fabricmc", "fabric-loom", "0.7-SNAPSHOT")
+	implementation("io.github.juuxel", "loom-quiltflower", "1.1.0")
+	implementation("com.github.PepperCode1", "mcp-tiny", "1bde92310cd80f0d834403d40219a2a06be431da")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,13 @@
 pluginManagement {
-    repositories {
-        maven("https://maven.fabricmc.net/") {
-            name = "Fabric"
-        }
-        gradlePluginPortal()
-    }
+	repositories {
+		maven("https://maven.fabricmc.net/") {
+			name = "Fabric"
+		}
+		maven("https://server.bbkr.space/artifactory/libs-release/") {
+			name = "Cotton"
+		}
+		gradlePluginPortal()
+	}
 }
 
 rootProject.name = "Create-Refabricated"
